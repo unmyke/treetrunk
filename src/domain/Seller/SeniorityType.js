003839ac0bb4c6a8fdf4a5687ac0b35898cdbbbf
Tuyana Dondokova <tuyana.dondokova@gmail.com>
2018-04-26 10:31:36 +0800

    Add SeniorityType tests

--- src/domain/Seller/SeniorityType.js
+++ src/domain/Seller/SeniorityType.js
@@ -2,6 +2,7 @@ import { BaseEntity } from '../_lib/BaseClasses';
 import { Day } from '../_lib/ValueObjects';
 import { SeniorityTypeId } from './SeniorityTypeId';
 import { Award } from './Award';
+import { addErrorDefinitionProperty } from 'src/infra/support/addErrorDefinition';
 
 export class SeniorityType extends BaseEntity {
   constructor({ seniorityTypeId = new SeniorityTypeId(), name, months }) {
@@ -12,18 +13,21 @@ export class SeniorityType extends BaseEntity {
   }
 
   addAward(value, day = new Day()) {
+    const previuosAward = this.getAwardAt(day);
     const award = new Award({ value, day });
+    if (previuosAward === award) {
+      throw this.constructor.errorDuplication;
+    }
     this.awards = [...this.awards, award].sort((a, b) => a.day > b.day);
   }
 
-  deleteAward(value, day = new Day()) {
-    const awardToDelete = new Award({ value, day });
-    const filteredAwards = this.awards.filter((award) =>
-      award.equals(awardToDelete)
+  deleteAward(value, day) {
+    const seniorityTypeToDelete = new Award({ value, day });
+    const filteredAwards = this.awards.filter(
+      (award) => !award.equals(seniorityTypeToDelete)
     );
-
-    if (filteredAwards.length === this.awards.length) {
-      throw new Error('NotFound');
+    if (this.awards.length === filteredAwards.length) {
+      this.constructor.errorNoAwards;
     }
 
     this.awards = filteredAwards;
@@ -33,4 +37,40 @@ export class SeniorityType extends BaseEntity {
     this.deleteAward(value, day);
     this.addAward(updateValue, updateDay);
   }
+
+  getAwardAt(day = new Day()) {
+    if (!this.hasAward(day)) {
+      return;
+    }
+
+    const [firstAward, ...restAwards] = this.awards;
+
+    const { value } = restAwards.reduce((currentAward, appointment) => {
+      return appointment.day <= day ? appointment : currentAward;
+    }, firstAward);
+
+    return value;
+  }
+
+  hasAward(day) {
+    const [firstAward] = this.awards;
+    return !!firstAward && firstAward.day <= day;
+  }
+
+  get currentAward() {
+    return this.getAwardAt();
+  }
 }
+
+addErrorDefinitionProperty(
+  SeniorityType,
+  'errorDuplication',
+  'OperationError',
+  'SeniorityType already have this award'
+);
+addErrorDefinitionProperty(
+  SeniorityType,
+  'errorNoAwards',
+  'OperationError',
+  'SeniorityType have not such award'
+);

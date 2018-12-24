25a1d7dd8445796d28760628dc9191d431522d4f
unmyke <unmyke@gmail.com>
2018-12-24 22:15:29 +0800

    wip: add base webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
-module.exports = {
-  entry: {
-    server: './src/client',
-    client: './src/server',
-  },
-  output: {
-    filename: '[name].js',
-    path: __dirname + '/dist',
-  },
+const path = require('path');
+const server = require('./webpack/server.config.js');
+const client = require('./webpack/client.config.js');
+
+function merge(config1, config2) {
+  return Object.assign({}, config1, config2);
+}
+
+const OUTPUT_DIR = path.resolve(__dirname, 'dist');
+
+const commonConfig = {
+  context: path.resolve(__dirname, 'src'),
+};
+
+module.exports = function(env, argv) {
+  return new Promise((resolve, reject) => {
+    return resolve({
+      entry: {
+        server: './server',
+        client: './client',
+      },
+      output: {
+        filename: '[name].bundle.js',
+        path: OUTPUT_DIR,
+      },
+    });
+  });
 };

f1729db49ece13807095a51f59cbc09204cf73b6
unmyke <unmyke@gmail.com>
2019-01-18 10:27:01 +0800

    wip

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -8,6 +8,13 @@ const rootDirectory = path.resolve(__dirname, '..');
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   plugins: [new CleanWebpackPlugin([outputDirectory], { root: rootDirectory })],
+  loaders: [
+    {
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    },
+  ],
   resolve: {
     alias: {
       '@config': path.resolve(__dirname, '../config'),

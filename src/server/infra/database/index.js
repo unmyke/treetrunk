76d4577c1fe5488a61f15f4d78acadfc14b0f8e8
unmyke <unmyke@gmail.com>
2019-04-11 07:52:37 +0800

    fix(db): use config injecting by webpack

--- src/server/infra/database/index.js
+++ src/server/infra/database/index.js
@@ -2,21 +2,20 @@ import { Database } from 'mongorito';
 import * as models from './models';
 import modelLoader from './model-loader';
 
-export default ({ config, errors }) => {
+export default ({ config: { database: config }, errors }) => {
   const getUrl = ({ db, host, port }) => {
     const protocol = 'mongodb';
 
     return `${protocol}://${host}:${port}/${db}`;
   };
 
-  const { db: dbConfig } = config;
-  if (!dbConfig) {
+  if (!config) {
     throw errors.databaseError(
       'Database config file not found, disabling database.'
     );
   }
 
-  const { host, port, db, user, pass, ...options } = dbConfig;
+  const { host, port, db, user, pass, ...options } = config;
   const auth = user ? { user, password: pass } : undefined;
   const url = getUrl({ host, port, db });
   const dbOptions = { auth, ...options };

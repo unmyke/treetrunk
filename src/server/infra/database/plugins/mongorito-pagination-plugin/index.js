fb04f52eb066bcf8ead4918b5bbb43a40d7893af
unmyke <unmyke@gmail.com>
2019-04-11 18:33:49 +0800

    refactor(infra): remove useless entry point

--- src/server/infra/database/plugins/mongorito-pagination-plugin/index.js
+++ src/server/infra/database/plugins/mongorito-pagination-plugin/index.js
@@ -1,1 +1,21 @@
-export { default } from './mongorito-pagination-plugin';
+import getPaginationOptions, { types } from './get-pagination-options';
+import { getCursorPaginagtion, getOffsetPaginagtion } from './pagination';
+
+export default (Model) => {
+  Model.getList = (query = {}) => {
+    const { options, type } = getPaginationOptions(query);
+
+    console.log(options, type);
+
+    switch (type) {
+      case types.CURSOR:
+        return getCursorPaginagtion(options);
+
+      case types.OFFSET:
+        return getOffsetPaginagtion(options);
+
+      default:
+        throw new Error('Invalid query');
+    }
+  };
+};

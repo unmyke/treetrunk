eabede1f0d486f164c99ef3e129783e6d9563033
unmyke <unmyke@gmail.com>
2019-04-16 06:34:53 +0800

    chore(gulp): use promise as dev tasks

--- gulpfile.js/dev.js
+++ gulpfile.js/dev.js
@@ -9,9 +9,9 @@ const {
 const getWatchTask = require('./lib/get-watch-task');
 const getDevServerTask = require('./lib/get-dev-server-task');
 
-const devServer = (done) => getWatchTask(SERVER, done);
-const devConsole = (done) => getWatchTask(CONSOLE, done);
-const devClient = (done) => getDevServerTask(CLIENT, done);
+const devServer = () => getWatchTask(SERVER);
+const devConsole = () => getWatchTask(CONSOLE);
+const devClient = () => getDevServerTask(CLIENT);
 const dev = parallel(devServer, devClient);
 
 module.exports = {

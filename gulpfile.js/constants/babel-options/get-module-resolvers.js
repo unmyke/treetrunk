350fe416d87019445d69a2c9d72ea9b7b5c4bffd
unmyke <unmyke@gmail.com>
2019-04-15 19:12:34 +0800

    chore(gulp): add module-resolver plugin opts

--- gulpfile.js/constants/babel-options/get-module-resolvers.js
+++ gulpfile.js/constants/babel-options/get-module-resolvers.js
@@ -1,41 +1,32 @@
-const { resolve, relative, normalize } = require('path');
+const { resolve } = require('path');
 
-const {
-  dirs: { SRC },
-} = require('../types');
-const { [SRC]: srcDir } = require('../dirs');
 const aliases = require('../aliases');
 
 module.exports = (target) => {
-  const root = `./${resolve(srcDir, target)}`;
-  const targetAliases = aliases[target];
   const config = resolve(__dirname, 'config', target);
+  const targetAliases = aliases[target];
 
-  const getPlugin = ({ alias, root }) => [
-    'module-resolver',
-    { root: `./${relative(process.cwd(), root)}`, alias },
-  ];
+  const getPlugin = (alias) => ['module-resolver', { alias }];
 
-  const getAlias = ({ root, targetAliases, aliasKey }) => {
+  const getAlias = ({ targetAliases, aliasKey }) => {
     if (aliasKey === '@config') {
       return { [aliasKey]: () => config };
     }
-    const alias = normalize(relative(root, targetAliases[aliasKey]));
+    const alias = targetAliases[aliasKey];
 
     const key = `^${aliasKey}(.*)$`;
-    const value = `./${alias}\\1`;
+    const value = `${alias}\\1`;
 
     return { [key]: value };
   };
 
-  return getPlugin({
-    alias: Object.keys(targetAliases).reduce(
+  return getPlugin(
+    Object.keys(targetAliases).reduce(
       (prevAliases, aliasKey) => ({
         ...prevAliases,
-        ...getAlias({ root, targetAliases, aliasKey }),
+        ...getAlias({ targetAliases, aliasKey }),
       }),
       {}
-    ),
-    root,
-  });
+    )
+  );
 };

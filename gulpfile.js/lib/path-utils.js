9a44b6a71bfbdcb530c8757d2d3538c5cc5f01be
unmyke <unmyke@gmail.com>
2019-04-15 15:35:44 +0800

    chore(gulp): add optional arg to getSrc and getDst
    getSrc and getDst takes optional object as last argument, that must
    contains only one prop - 'absolute'. If passed it returns absolute path.
    Default value { absolute: false }

--- gulpfile.js/lib/path-utils.js
+++ gulpfile.js/lib/path-utils.js
@@ -1,10 +1,32 @@
-const { resolve } = require('path');
+const { resolve, relative, normalize } = require('path');
 const {
   dirs: { DST, SRC },
 } = require('../constants/types');
 const { [DST]: dstDir, [SRC]: srcDir } = require('../constants/dirs');
 
-const getPath = (dir) => (...target) => resolve(dir, ...target);
+const rootDir = process.cwd();
+
+const parseArgs = (args) => {
+  const options = args[args.length - 1];
+  if (options instanceof Object) {
+    return {
+      paths: args.slice(0, -1),
+      absolute: !!options.absolute,
+    };
+  }
+
+  return { paths: [...args], absolute: false };
+};
+
+const getPath = (dir) => (...args) => {
+  const { paths, absolute } = parseArgs(args);
+  const relativePath = `./${normalize(
+    relative(rootDir, resolve(dir, ...paths))
+  )}`;
+
+  if (absolute) return resolve(relativePath);
+  return relativePath;
+};
 
 module.exports = {
   getDstPath: getPath(dstDir),

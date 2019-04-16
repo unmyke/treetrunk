a761e128c1c40efdb153dace3c30e33c0e9c4193
unmyke <unmyke@gmail.com>
2019-04-16 15:20:46 +0800

    chore(test): fix imports

--- jest.config/module-name-mapper.js
+++ jest.config/module-name-mapper.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
-const alias = require('../gulpfile.js/constants/alias/');
+const aliases = require('../gulpfile.js/constants/aliases/');
 
 const configKey = '@config';
 
@@ -17,7 +17,7 @@ const getAlias = (aliasKey, targetAlias) => {
   };
 };
 
-const names = Object.values(alias).reduce((prevTargetMapper, targetAlias) => {
+const names = Object.values(aliases).reduce((prevTargetMapper, targetAlias) => {
   return Object.keys(targetAlias).reduce(
     (prevMapper, aliasKey) => ({
       ...prevMapper,

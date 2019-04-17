87f75cf15f4a5056a12de32eaf7083a4195cad36
unmyke <unmyke@gmail.com>
2019-04-17 16:34:00 +0800

    test(factory): refactor factory models (mongorito)

--- src/server/infra/support/test/factories/seller-management/post.js
+++ src/server/infra/support/test/factories/seller-management/post.js
@@ -1,53 +1,18 @@
+import { getDaysSequence } from '../_lib/day-utils';
+
 const Post = (factory, { Post }) => {
-  factory.define(
-    'post',
-    Post,
-    {
+  factory.define('post', Post, ({ pieceRatesCount = 1 }) => {
+    return {
       postId: factory.chance('guid', { version: 4 }),
       name: factory.chance('word', { length: 40 }),
       state: factory.chance('pickone', ['active', 'deleted']),
-    },
-    {
-      async afterCreate(post, attrs, { pieceRatesCount } = {}) {
-        if (
-          pieceRatesCount === 0 &&
-          attrs.pieceRates &&
-          attrs.pieceRates.length === 0
-        ) {
-          return post.reload({ include: ['pieceRates'] });
-        }
-
-        const pieceRateAttrs = {
-          postId: post.postId,
-        };
-
-        const pieceRateFactoryArgs = ['postPieceRate'];
-
-        switch (true) {
-          case pieceRatesCount !== undefined && pieceRatesCount > 0:
-            pieceRateFactoryArgs.push(pieceRatesCount, pieceRateAttrs);
-            break;
-
-          case attrs.pieceRates !== undefined && attrs.pieceRates.length > 0:
-            pieceRateFactoryArgs.push(
-              attrs.pieceRates.map((attr) => ({
-                ...attr,
-                ...pieceRateAttrs,
-              }))
-            );
-            break;
-
-          default:
-            pieceRateFactoryArgs.push(3, pieceRateAttrs);
-            break;
-        }
-
-        return factory
-          .createMany(...pieceRateFactoryArgs)
-          .then(() => post.reload({ include: ['pieceRates'] }));
-      },
-    }
-  );
+      pieceRates: () =>
+        getDaysSequence({ count: pieceRatesCount }).map((day) => ({
+          value: factory.chance('floating', { fixed: 1, min: 0, max: 20 }),
+          day,
+        })),
+    };
+  });
 };
 
 export default Post;
--- src/server/infra/support/test/factories/seller-management/seller.js
+++ src/server/infra/support/test/factories/seller-management/seller.js
@@ -1,66 +1,25 @@
 import { getDaysSequence } from '../_lib/day-utils';
 
 const Seller = (factory, { Seller }) => {
-  factory.define(
-    'seller',
-    Seller,
-    ({ appointmentsCount = 1 }) => {
-      return {
-        sellerId: factory.chance('guid', { version: 4 }),
-        lastName: factory.chance('last'),
-        firstName: factory.chance('name'),
-        middleName: factory.chance('name', { middle: true }),
-        phone: factory.chance('phone'),
-        state: 'recruited',
-        appointments: () =>
-          getDaysSequence({ count: appointmentsCount }).map((day) => ({
-            postId: factory.assoc('Post'),
-            day,
-          })),
-      };
-    },
-    {
-      async afterCreate(seller, attrs, { appointmentsCount } = {}) {
-        if (
-          appointmentsCount === 0 &&
-          attrs.appointments &&
-          attrs.appointments.length === 0
-        ) {
-          return seller.reload({ include: ['appointments'] });
-        }
+  factory.define('seller', Seller, ({ appointmentsCount = 1 }) => {
+    return {
+      sellerId: factory.chance('guid', { version: 4 }),
+      lastName: factory.chance('last'),
+      firstName: factory.chance('name'),
+      middleName: factory.chance('name', { middle: true }),
+      phone: factory.chance('phone'),
+      state: 'recruited',
+      appointments: () => {
+        const days = getDaysSequence({ count: appointmentsCount });
+        console.log(days);
 
-        const appointmentAttrs = {
-          sellerId: seller.sellerId,
-        };
-
-        const appointmentFactoryArgs = ['sellerAppointment'];
-
-        switch (true) {
-          case appointmentsCount !== undefined && appointmentsCount > 0:
-            appointmentFactoryArgs.push(appointmentsCount, appointmentAttrs);
-            break;
-
-          case attrs.appointments !== undefined &&
-            attrs.appointments.length > 0:
-            appointmentFactoryArgs.push(
-              attrs.appointments.map((attr) => ({
-                ...attr,
-                ...appointmentAttrs,
-              }))
-            );
-            break;
-
-          default:
-            appointmentFactoryArgs.push(3, appointmentAttrs);
-            break;
-        }
-
-        return factory
-          .createMany(...appointmentFactoryArgs)
-          .then(() => seller.reload({ include: ['appointments'] }));
+        return days.map((day) => ({
+          postId: factory.assoc('post', 'postId'),
+          day,
+        }));
       },
-    }
-  );
+    };
+  });
 };
 
 export default Seller;

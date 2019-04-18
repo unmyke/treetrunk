bd2afbc76a0c97529c6761df94ca3392d8523a65
unmyke <unmyke@gmail.com>
2019-04-18 21:25:33 +0800

    fix(infra): fix repo#save method

--- src/server/infra/database/plugins/mongorito-save-plugin.js
+++ src/server/infra/database/plugins/mongorito-save-plugin.js
@@ -1,22 +1,34 @@
 /* eslint-disable no-underscore-dangle */
 import { ActionTypes } from 'mongorito';
 
-export default () => {
-  return (store) => (next) => (action) => {
-    if (action.type === ActionTypes.SAVE) {
-      const { model, modelClass } = store;
-      const idPropName = `${modelClass.name.toLowerCase()}Id`;
-      const id = model ? model[idPropName] : null;
+import getIdPropName from './get-id-prop-name';
 
-      if (id) {
-        modelClass.find({ [idPropName]: id }).then((existingModel) => {
-          if (existingModel) {
-            model.set({ _id: existingModel._id });
-          }
-        });
-      }
+const { SAVE, CREATE, UPDATE } = ActionTypes;
+
+export default () => {
+  return ({ modelClass, dispatch, getState }) => (next) => (action) => {
+    if (action.type !== SAVE) {
+      return next(action);
     }
 
-    return next(action);
+    const idPropName = getIdPropName(modelClass);
+    const { [idPropName]: id, ...modelFields } = getState().fields;
+
+    return modelClass.findOne({ [idPropName]: id }).then((existingModel) => {
+      if (existingModel) {
+        const { _id, ...existingModelFields } = existingModel.get();
+        const newModelFields = {
+          ...existingModelFields,
+          ...modelFields,
+          _id,
+        };
+
+        return dispatch({ type: UPDATE, fields: newModelFields });
+      }
+      return dispatch({
+        type: CREATE,
+        fields: { ...modelFields, [idPropName]: id },
+      });
+    });
   };
 };

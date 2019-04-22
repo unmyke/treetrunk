cfb5d0e246e667e94876ad7ebc0e53c9af24aac9
unmyke <unmyke@gmail.com>
2019-04-22 11:47:57 +0800

    test(factory): fix generation of list props

--- src/server/infra/support/test/factories/seller-management/seller.js
+++ src/server/infra/support/test/factories/seller-management/seller.js
@@ -1,22 +1,24 @@
 import { getDaysSequence, getRandomDay } from '../_lib/day-utils';
 
 const Seller = (factory, { Seller }) => {
-  factory.define('seller', Seller, ({ appointmentsCount = 1 }) => {
-    return {
-      sellerId: factory.chance('guid', { version: 4 }),
-      lastName: factory.chance('last'),
-      firstName: factory.chance('name'),
-      middleName: factory.chance('name', { middle: true }),
-      phone: factory.chance('phone'),
-      state: 'recruited',
-      appointments: () =>
-        getDaysSequence({ count: appointmentsCount }).map((day) => ({
-          postId: factory.assoc('post', 'postId'),
-          day,
-        })),
-      createdAt: () => getRandomDay(),
-    };
-  });
+  factory.define('seller', Seller, ({ appointmentsCount = 1 }) => ({
+    sellerId: factory.chance('guid', { version: 4 }),
+    lastName: factory.chance('last'),
+    firstName: factory.chance('name'),
+    middleName: factory.chance('name', { middle: true }),
+    phone: factory.chance('phone'),
+    state: 'recruited',
+    appointments: () =>
+      factory
+        .assoc('post', 'postId')()
+        .then((postId) =>
+          getDaysSequence({ count: appointmentsCount }).map((day) => ({
+            postId,
+            day,
+          }))
+        ),
+    createdAt: () => getRandomDay(),
+  }));
 };
 
 export default Seller;

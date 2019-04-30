ce9089da4c7ccfe00ff20d60ce42dbbae822e197
unmyke <unmyke@gmail.com>
2019-04-30 12:20:40 +0800

    feat(infra): add #getByMonth to SeniorityType model

--- src/server/infra/database/models/seniority-type.js
+++ src/server/infra/database/models/seniority-type.js
@@ -1,3 +1,13 @@
 import BaseModel from './base-model';
 
 export default class SeniorityType extends BaseModel {}
+
+const extendSeniorityType = (SeniorityType) => {
+  SeniorityType.getByMonths = function getByMonths(months) {
+    return this.lte(months)
+      .sort({ months: -1 })
+      .findOne();
+  };
+};
+
+SeniorityType.use(extendSeniorityType);

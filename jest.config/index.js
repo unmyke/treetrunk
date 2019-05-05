653f273acf3c3522e665ed761b4962e341ecc272
unmyke <unmyke@gmail.com>
2019-05-06 00:04:35 +0800

    test(jest): add tests matcher

--- jest.config/index.js
+++ jest.config/index.js
@@ -4,5 +4,9 @@ module.exports = {
   rootDir: process.cwd(),
   testURL: 'http://localhost',
   setupFiles: ['jest-plugin-context/setup'],
+  testMatch: [
+    '**/?(*.)+(spec|test).[tj]s?(x)',
+    '**/*.test/**/!(imports)*.[tj]s?(x)',
+  ],
   moduleNameMapper,
 };

1d699adaff02aecd71e8b30e719d8fe2a8703105
unmyke <unmyke@gmail.com>
2019-05-06 00:52:08 +0800

    fix(domain): add deletedAt timestamp to BaseEnity

--- src/server/domain/_lib/base-entity/base-entity.js
+++ src/server/domain/_lib/base-entity/base-entity.js
@@ -3,12 +3,13 @@ import { applyFSM, getIdPropName } from '../base-methods';
 import BaseClass from '../base-class';
 
 export default class BaseEntity extends BaseClass {
-  constructor({ id, createdAt = new Date(), updatedAt }) {
+  constructor({ id, createdAt = new Date(), updatedAt, deletedAt }) {
     super();
     const idPropName = getIdPropName(id);
     this[idPropName] = id;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
 
     applyFSM(this.constructor);
     this._fsm();

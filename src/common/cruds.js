9492c5765c288f8e1e2cd8f1475dc5d38dc3ae1a
unmyke <unmyke@gmail.com>
2019-05-10 17:11:22 +0800

    feat(common): add sharable CRUD names

---
+++ src/common/cruds.js
@@ -0,0 +1,12 @@
+const CRUDS = {
+  GET: 'get',
+  GET_LIST: 'getList',
+  CREATE: 'create',
+  CREATE_MANY: 'createMany',
+  UPDATE: 'update',
+  UPDATE_MANY: 'updateMany',
+  DELETE: 'delete',
+  DELETE_MANY: 'deleteMany',
+};
+
+export default CRUDS;

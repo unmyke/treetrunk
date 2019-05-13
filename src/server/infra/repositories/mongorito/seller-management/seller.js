586b9eb30457f42587f51b35fa631ac0df48db85
unmyke <unmyke@gmail.com>
2019-05-13 16:03:33 +0800

    fix(infra): fix repos use moved base repo

--- src/server/infra/repositories/mongorito/seller-management/seller.js
+++ src/server/infra/repositories/mongorito/seller-management/seller.js
@@ -1,4 +1,4 @@
-import BaseRepository from './base';
+import BaseRepository from '../base';
 
 const SellerRepository = ({ Model, mapper, errors }) => {
   const baseRepo = BaseRepository({ Model, mapper, errors });

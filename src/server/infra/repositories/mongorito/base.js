ce979b8e789526eacd6f6b487de6126ffb9cae95
unmyke <unmyke@gmail.com>
2019-05-13 15:51:05 +0800

    refactor(infra): use refactored Model#getList

--- src/server/infra/repositories/mongorito/base.js
+++ src/server/infra/repositories/mongorito/base.js
@@ -1,3 +1,5 @@
+const getIdValue = ({ value }) => value;
+
 const BaseRepository = ({ Model, mapper, errors }) => {
   const mapToEntity = (model) => (model ? mapper.toEntity(model.get()) : null);
   const throwError = (error) => {
@@ -7,29 +9,30 @@ const BaseRepository = ({ Model, mapper, errors }) => {
   const idPropName = `${Model.name.toLowerCase()}Id`;
 
   const findModel = (id) =>
-    Model.findOne({
-      [idPropName]: id.value,
-    }).then((model) => {
-      if (!model)
-        throw errors.modelNotFound(
-          `${Model.name} with ${idPropName}: "${id}" not found`
-        );
-      return model;
-    });
+    Model.where(idPropName)
+      .equals(getIdValue(id))
+      .findOne()
+      .then((model) => {
+        if (!model)
+          throw errors.modelNotFound(
+            `${Model.name} with ${idPropName}: "${getIdValue(id)}" not found`
+          );
+        return model;
+      });
 
-  const getList = (query) =>
-    Model.getList(query).then(
-      ({ result, cursor, hasMore }) => ({
-        result: result.map(mapToEntity),
-        cursor,
-        hasMore,
+  const getList = ({ id = {}, ...query } = {}) =>
+    Model.getList({ id: getIdValue(id), ...query }).then(
+      ({ models, hasBefore, hasAfter }) => ({
+        entities: models.map(mapToEntity),
+        hasBefore,
+        hasAfter,
       }),
       throwError
     );
   const getById = (id) => findModel(id).then(mapToEntity, throwError);
   const getByIds = (ids) =>
     Model.where(idPropName)
-      .in(ids.map(({ value }) => value))
+      .in(ids.map(getIdValue))
       .find()
       .then((models) => models.map(mapToEntity), throwError);
   const getOne = (query) => Model.findOne(query).then(mapToEntity, throwError);

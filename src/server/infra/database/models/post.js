f8ae681ae524dc70e8bf15c43549c36fcc555f1b
unmyke <unmyke@gmail.com>
2019-05-13 13:12:27 +0800

    feat(infra): add text filter fields getter

--- src/server/infra/database/models/post.js
+++ src/server/infra/database/models/post.js
@@ -1,3 +1,11 @@
 import BaseModel from './base-model';
 
 export default class Post extends BaseModel {}
+
+const extendPost = (Post) => {
+  Post.textFilterFields = function textFilterFields() {
+    return ['name'];
+  };
+};
+
+Post.use(extendPost);

8f678a9920fe201f48da7c52c595f4aaa300a440
unmyke <unmyke@gmail.com>
2019-05-15 12:17:45 +0800

    fix(infra): fix forgotten import

--- src/server/infra/_lib/get-id-prop-name-by-model.js
+++ src/server/infra/_lib/get-id-prop-name-by-model.js
@@ -1,3 +1,4 @@
-const getIdPropName = (Model) => `${lowerFirst(Model.name)}Id`;
+import { lowerFirst } from 'lodash/fp';
 
+const getIdPropName = (Model) => `${lowerFirst(Model.name)}Id`;
 export default getIdPropName;

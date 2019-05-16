602ffb15e30f54671049310738c80907247fe200
unmyke <unmyke@gmail.com>
2019-05-16 23:42:28 +0800

    refactor(app): generate boilerplated CRUD services

---
+++ src/server/app/get-crud-services/index.js
@@ -0,0 +1,22 @@
+import { CRUDS } from '@common';
+import * as crudServices from './crud-services';
+import getServiceName from '../get-crud-service-name';
+
+const getCrudServices = (EntityName, { entities, commonTypes, repositories }) =>
+  Object.values(CRUDS).reduce((prevServices, crudName) => {
+    const crudServiceGererator = crudServices[crudName];
+    const crudService =
+      crudServiceGererator &&
+      crudServiceGererator(EntityName)({
+        entities,
+        commonTypes,
+        repositories,
+      });
+
+    return {
+      ...prevServices,
+      [getServiceName({ EntityName, crudName })]: crudService,
+    };
+  }, {});
+
+export default getCrudServices;

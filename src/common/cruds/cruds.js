9a1818b5fc5f62673fc96a3c125a62a37f687bfb
unmyke <unmyke@gmail.com>
2019-05-16 23:41:16 +0800

    refactor(common): add crudPredicates into CRUDS constant

---
+++ src/common/cruds/cruds.js
@@ -0,0 +1,8 @@
+export const GET = 'get';
+export const GET_LIST = 'getList';
+export const CREATE = 'create';
+// export const CREATE_MANY = 'createMany';
+export const UPDATE = 'update';
+// export const UPDATE_MANY = 'updateMany';
+export const DELETE = 'delete';
+// export const DELETE_MANY ='deleteMany';

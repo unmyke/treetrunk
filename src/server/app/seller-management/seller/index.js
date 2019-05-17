994133b20e60babb11d116ab961b6bc2aac4aa40
unmyke <unmyke@gmail.com>
2019-05-17 22:39:10 +0800

    feat(app): export Seller services

--- src/server/app/seller-management/seller/index.js
+++ src/server/app/seller-management/seller/index.js
@@ -1,3 +1,19 @@
 import CreateSellerAppointment from './create-appointment';
+import Create from './create';
+import DeleteAppointment from './delete-appointment';
+import DeleteDismiss from './delete-dismiss';
+import Dismiss from './dismiss';
+import UpdateAppointment from './update-appointment';
+import UpdateDismiss from './update-dismiss';
+import Update from './update';
 
-export default { CreateSellerAppointment };
+export default {
+  Create,
+  Update,
+  CreateSellerAppointment,
+  UpdateAppointment,
+  DeleteAppointment,
+  Dismiss,
+  UpdateDismiss,
+  DeleteDismiss,
+};

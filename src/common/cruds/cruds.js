3b85ba7ece31f579c7081165b166b5e92d324443
unmyke <unmyke@gmail.com>
2019-05-17 22:36:28 +0800

    feat(common): add restore and destroy to standard CRUD operations

--- src/common/cruds/cruds.js
+++ src/common/cruds/cruds.js
@@ -6,3 +6,7 @@ export const UPDATE = 'update';
 // export const UPDATE_MANY = 'updateMany';
 export const DELETE = 'delete';
 // export const DELETE_MANY ='deleteMany';
+export const RESTORE = 'restore';
+// export const RESTORE_MANY ='restoreMany';
+export const DESTROY = 'destroy';
+// export const DESTROY_MANY ='destroyMany';

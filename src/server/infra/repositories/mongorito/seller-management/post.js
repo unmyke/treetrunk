db8b8dc25d28aa6c68af11a867d374a77e16d94e
unmyke <unmyke@gmail.com>
2019-05-19 23:59:54 +0800

    fix(infra): fix postRepo#getList return wrong list
    Fix postRepo#getList return one less than must be

--- src/server/infra/repositories/mongorito/seller-management/post.js
+++ src/server/infra/repositories/mongorito/seller-management/post.js
@@ -1,13 +1,22 @@
+import { getIdValue } from '@infra/_lib';
 import BaseRepository from '../base';
 
-const PostRepository = ({ Entity, Model, mapper, errors }) => {
+const PostRepository = ({
+  commonTypes: { PostId },
+  Entity,
+  Model,
+  mapper,
+  errors,
+}) => {
   const baseRepo = BaseRepository({ Entity, Model, mapper, errors });
 
   const getList = (opts) =>
-    baseRepo.getList(opts).then(({ entities, ...meta }) => ({
-      entities: entities.filter((post) => !post.isDismissPost()),
-      ...meta,
-    }));
+    baseRepo
+      .getList(opts, { postId: { $ne: getIdValue(PostId.dismissPostId) } })
+      .then(({ entities, ...meta }) => ({
+        entities: entities.filter((post) => !post.isDismissPost()),
+        ...meta,
+      }));
 
   return Object.freeze({
     ...baseRepo,

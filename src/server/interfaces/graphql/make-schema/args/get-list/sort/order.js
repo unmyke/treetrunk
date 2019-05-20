8c5d62e3a600aa8895f9211c109d2f347a2ee49b
unmyke <unmyke@gmail.com>
2019-05-20 18:00:06 +0800

    refactor(interface): define graphql enums as obj

--- src/server/interfaces/graphql/make-schema/args/get-list/sort/order.js
+++ src/server/interfaces/graphql/make-schema/args/get-list/sort/order.js
@@ -3,7 +3,10 @@ import { enumType } from 'nexus';
 const OrderEnum = enumType({
   name: 'OrderEnum',
   description: 'Order values',
-  members: ['asc', 'desc'],
+  members: {
+    ASC: 'asc',
+    DESC: 'desc',
+  },
 });
 
 export default OrderEnum;

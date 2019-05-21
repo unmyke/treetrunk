f72cb35cb953dcbb2727c2725e698ec030ebb535
unmyke <unmyke@gmail.com>
2019-05-22 00:31:24 +0800

    feat(infra): add autoconnect to database

--- src/server/infra/database/plugins/index.js
+++ src/server/infra/database/plugins/index.js
@@ -1,5 +1,6 @@
 export { default as savePlugin } from './mongorito-save-plugin';
 export { default as listPlugin } from './mongorito-list-plugin';
+export { default as autoConnectPlugin } from './mongorito-auto-connect';
 export {
   default as entityIdIndexesPlugin,
 } from './mongorito-entity-id-indexes-plugin';

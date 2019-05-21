9a496ba22a9fe5476b6fd406d42554d670baec2c
unmyke <unmyke@gmail.com>
2019-05-21 16:28:05 +0800

    fix(app): remove unnecessary "asyn" in create-post-piece-rate

--- src/server/app/seller-management/post/create-piece-rate.js
+++ src/server/app/seller-management/post/create-piece-rate.js
@@ -1,7 +1,7 @@
 const CreatePieceRate = ({
   repositories: { Post: postRepo },
   commonTypes: { PostId, Day },
-}) => async (postIdValue, { value, day: date }) =>
+}) => (postIdValue, { value, day: date }) =>
   postRepo.getById(new PostId({ value: postIdValue })).then((post) => {
     post.addPieceRate(value, new Day({ value: new Date(date) }));
 

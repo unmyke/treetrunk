9957223d12c80469b60d0b75dd3cd8b1540e2b53
unmyke <unmyke@gmail.com>
2019-05-22 17:16:29 +0800

    test(interfaces): add graphql schema tests #wip

--- src/server/interfaces/graphql/feature.test/index.js
+++ src/server/interfaces/graphql/feature.test/index.js
@@ -1,4 +1,4 @@
-import { omit } from 'lodash';
+import { startOfDay } from 'date-fns';
 
 import container from '@container';
 import { CRUDS } from '@common';
@@ -36,21 +36,65 @@ describe(`graphQl endpoint`, () => {
       describe(`#seller`, () => {
         beforeEach(() =>
           factory.create('seller').then((s) => {
-            sellerModel = s;
+            sellerModel = s.get();
           })
         );
         operationName = queries.GET_SELLER_BY_ID;
 
-        context('if pass ID', () => {
+        context('if pass existent ID', () => {
           test(`should return seller with corresponding id`, () =>
             query({
               query: operationName,
-              variables: { id: sellerModel.get('sellerId') },
+              variables: { id: sellerModel.sellerId },
             }).then(({ data, errors }) => {
               expect(errors).toBeUndefined();
               expect(data).toHaveProperty('seller');
               const { seller } = data;
-              expect(seller).toEqual(mappers.Seller(seller));
+              expect(seller.id).toEqual(sellerModel.sellerId);
+              expect(seller.firstName).toBe(sellerModel.firstName);
+              expect(seller.middleName).toBe(sellerModel.middleName);
+              expect(seller.lastName).toBe(sellerModel.lastName);
+              expect(seller.appointments).toHaveLength(
+                sellerModel.appointments.length
+              );
+              expect(seller.appointments[0].postId).toBe(
+                sellerModel.appointments[0].postId
+              );
+              expect(seller.appointments[0].day).toBe(
+                startOfDay(sellerModel.appointments[0].day).getTime()
+              );
+              expect(seller.postId).toBe(sellerModel.appointments[0].postId);
+              expect(seller.postIds).toHaveLength(
+                sellerModel.appointments.length
+              );
+            }));
+        });
+        context('if pass unexistent ID', () => {
+          test(`should return seller with corresponding id`, () =>
+            query({
+              query: operationName,
+              variables: { id: sellerModel.sellerId },
+            }).then(({ data, errors }) => {
+              expect(errors).toBeUndefined();
+              expect(data).toHaveProperty('seller');
+              const { seller } = data;
+              expect(seller.id).toEqual(sellerModel.sellerId);
+              expect(seller.firstName).toBe(sellerModel.firstName);
+              expect(seller.middleName).toBe(sellerModel.middleName);
+              expect(seller.lastName).toBe(sellerModel.lastName);
+              expect(seller.appointments).toHaveLength(
+                sellerModel.appointments.length
+              );
+              expect(seller.appointments[0].postId).toBe(
+                sellerModel.appointments[0].postId
+              );
+              expect(seller.appointments[0].day).toBe(
+                startOfDay(sellerModel.appointments[0].day).getTime()
+              );
+              expect(seller.postId).toBe(sellerModel.appointments[0].postId);
+              expect(seller.postIds).toHaveLength(
+                sellerModel.appointments.length
+              );
             }));
         });
       });

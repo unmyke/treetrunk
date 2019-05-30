b750604ebdced34b3e751dfae0efdb605270583e
unmyke <unmyke@gmail.com>
2019-05-30 12:48:24 +0800

    test(interfaces): add graphql schema tests #wip

--- src/server/interfaces/graphql/feature.test/index.js
+++ src/server/interfaces/graphql/feature.test/index.js
@@ -3,15 +3,18 @@ import getServerUrl from './get-server-url';
 import queryTests from './queries';
 import mutationTests from './mutations';
 import { queries, mutations } from './generators';
+import * as mocks from './mocks';
 
 const {
   config: { api },
   tests: {
-    interface: { getApolloClient },
+    interfaces: { getApolloClient },
   },
 } = container;
 
+const ctx = { getApolloClient, queries, mutations, mocks };
+
 describe(`GraphQL endpoint ${getServerUrl(api)}`, () => {
-  queryTests({ getApolloClient, queries, mutations });
-  mutationTests({ getApolloClient, queries, mutations });
+  queryTests(ctx);
+  mutationTests(ctx);
 });
